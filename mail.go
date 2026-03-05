864aa68bbf24f9cb58ba0d9a6bdc4470ad5b33cd
elmyrockers <elmyrockers@gmail.com>
2026-03-06 03:50:26 +0800

    Add 9 other methods similar to PHPMailer methods

--- mail.go
+++ mail.go
@@ -30,3 +30,41 @@ func New() *Mail {
         Debug:   0, // 0 = none
     }
 }
+
+func (m *Mail) SetFrom(email, name string) {
+    m.From = email
+    m.FromName = name
+}
+
+func (m *Mail) AddAddress(email string) {
+    m.To = append(m.To, email)
+}
+
+func (m *Mail) AddCC(email string) {
+    m.Cc = append(m.Cc, email)
+}
+
+func (m *Mail) AddBCC(email string) {
+    m.Bcc = append(m.Bcc, email)
+}
+
+func (m *Mail) AddReplyTo(email string) {
+    m.ReplyTo = append(m.ReplyTo, email)
+}
+
+func (m *Mail) SetSubject(subject string) {
+    m.Subject = subject
+}
+
+func (m *Mail) SetBody(body string) {
+    m.Body = body
+}
+
+func (m *Mail) SetAltBody(alt string) {
+    m.AltBody = alt
+}
+
+// Set debug level: 0 = none, 1 = errors only, 2 = verbose
+func (m *Mail) SetDebug(level int) {
+    m.Debug = level
+}
