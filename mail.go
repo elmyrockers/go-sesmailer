3f843aedc2ed8687de245f2b296321fe85fe769f
elmyrockers <elmyrockers@gmail.com>
2026-03-06 14:25:36 +0800

    Add methods chaining

--- mail.go
+++ mail.go
@@ -31,40 +31,49 @@ func New() *Mail {
     }
 }
 
-func (m *Mail) SetFrom(email, name string) {
+func (m *Mail) SetFrom(email, name string) *Mail {
     m.From = email
     m.FromName = name
+    return m
 }
 
-func (m *Mail) AddAddress(email string) {
+func (m *Mail) AddAddress(email string) *Mail {
     m.To = append(m.To, email)
+    return m
 }
 
-func (m *Mail) AddCC(email string) {
+func (m *Mail) AddCC(email string) *Mail {
     m.Cc = append(m.Cc, email)
+    return m
 }
 
-func (m *Mail) AddBCC(email string) {
+func (m *Mail) AddBCC(email string) *Mail {
     m.Bcc = append(m.Bcc, email)
+    return m
 }
 
-func (m *Mail) AddReplyTo(email string) {
+func (m *Mail) AddReplyTo(email string) *Mail {
     m.ReplyTo = append(m.ReplyTo, email)
+    return m
 }
 
-func (m *Mail) SetSubject(subject string) {
+func (m *Mail) SetSubject(subject string) *Mail {
     m.Subject = subject
+    return m
 }
 
-func (m *Mail) SetBody(body string) {
+func (m *Mail) SetBody(body string) *Mail {
     m.Body = body
+    return m
 }
 
-func (m *Mail) SetAltBody(alt string) {
+func (m *Mail) SetAltBody(alt string) *Mail {
     m.AltBody = alt
+    return m
 }
 
 // Set debug level: 0 = none, 1 = errors only, 2 = verbose
-func (m *Mail) SetDebug(level int) {
+func (m *Mail) SetDebug(level int) *Mail {
     m.Debug = level
+    return m
 }
