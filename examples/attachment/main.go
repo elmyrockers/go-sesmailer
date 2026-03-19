7a081ca20467e9212dd2ca721826329ace6c5771
elmyrockers <elmyrockers@gmail.com>
2026-03-19 12:18:36 +0800

    Include an example for attachments

---
+++ examples/attachment/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/elmyrockers/go-sesmailer"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	// Create mailer
+	sesmailer.New().
+		SetFrom("no-reply@xeno.com.my", "Xeno System").
+		AddAddress("elmyrockers@gmail.com", "Helmi Aziz").
+		SetSubject("Email With Attachments").
+		SetBody("This is test email with attachments").
+		AddAttachment( "cat.webp", "" ).
+		AddAttachment( "rabbit.jpg", "" ).
+		AddAttachment( "dog.jpg", "" ).
+		SetDebug(2).
+		Send()
+}
