b15f110dbba74717bfc8ff646b5402e2a69fc9e6
elmyrockers <elmyrockers@gmail.com>
2026-03-20 11:40:49 +0800

    Remove commented import: go-spew

--- mailer.go
+++ mailer.go
@@ -21,8 +21,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ses"
 	"github.com/aws/aws-sdk-go-v2/service/ses/types"
 	"github.com/aws/smithy-go/logging"
-
-	// "github.com/davecgh/go-spew/spew"
 )
 
 
