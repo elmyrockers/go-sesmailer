89646030e3de8ff735e6bb076da141345b883d85
elmyrockers <elmyrockers@gmail.com>
2026-07-11 01:07:35 +0800

    Update SetDebug() method to Dump() for debugging purpose

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"github.com/elmyrockers/go-sesmailer"
 	_ "github.com/joho/godotenv/autoload"
+	// "github.com/davecgh/go-spew/spew"
 )
 
 func main() {
 	// Create mailer
-	sesmailer.New().
-		SetFrom("no-reply@xeno.com.my", "Xeno System").
-		AddAddress("elmyrockers@gmail.com", "Helmi Aziz").
-		AddReplyTo("elmyrockers2@gmail.com", "Helmi Aziz 2").
-		SetSubject("Test subject 20").
-		SetBody("test body").
-		SetAltBody("test alt body").
-		SetDebug(2).
-		Send()
+		sesmailer.New().
+			SetFrom("no-reply@xeno.com.my", "Xeno System").
+			AddAddress("elmyrockers@gmail.com", "Helmi Aziz").
+			AddReplyTo("elmyrockers2@gmail.com", "Helmi Aziz 2").
+			SetSubject("Test subject 20").
+			SetBody("test body").AsHTML().
+			SetAltBody("test alt body").
+			Dump().
+			Send()
+		// spew.Dump( mail ).
 }
