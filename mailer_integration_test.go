16aa03d8dc9f08ea5496a42664aea3b0202444f7
elmyrockers <elmyrockers@gmail.com>
2026-07-11 18:49:05 +0800

    Add new test method: TestMailer_SendHtmlWithEmbeds

--- mailer_integration_test.go
+++ mailer_integration_test.go
@@ -63,7 +63,7 @@ func TestMailer_SendHtmlWithAttachments( t *testing.T ) {
 				require.NoError(t, err, "failed to read dog.jpg")
 
 	// Test: add mime headers and body then send
-		subject := fmt.Sprintf("sesmailer integration test - html+attachment - %d", time.Now().UnixNano())
+		subject := fmt.Sprintf("sesmailer integration test - html+attachments - %d", time.Now().UnixNano())
 		output, err := mailer.
 			SetFrom( "noreply@xeno.com.my", "Xeno System").
 			AddReplyTo( "info@xeno.com.my", "Xeno Admin" ).
@@ -72,7 +72,7 @@ func TestMailer_SendHtmlWithAttachments( t *testing.T ) {
 			AddBCC( "elmyrockers3@gmail.com", "Project Manager").
 			SetSubject(subject).
 
-			SetBody("<h1>Hello</h1><p>This email has an attachment.</p>").AsHTML().
+			SetBody("<h1>Hello</h1><p>This email has attachments.</p>").AsHTML().
 			SetAltBody("Hello. This is the plain text fallback.").
 			Attach("cat.webp", catImg).
 			Attach("rabbit.jpg", rabbitImg).
@@ -83,9 +83,42 @@ func TestMailer_SendHtmlWithAttachments( t *testing.T ) {
 	require.NotNil(t, output)
 	assert.NotEmpty(t, output.MessageId)
 
-	t.Logf("Sent HTML+attachment email, MessageId=%s", *output.MessageId)
+	t.Logf("Sent HTML+attachments email, MessageId=%s", *output.MessageId)
 }
 
 func TestMailer_SendHtmlWithEmbeds( t *testing.T ) {
+	// Test construct
+		mailer := New()
+		require.NotNil(t, mailer, "New() should return a non-nil Mailer")
 
+	// Get image data in bytes
+		catImg, err := os.ReadFile("examples/attachment/cat.webp")
+				require.NoError(t, err, "failed to read cat.webp")
+		rabbitImg, err := os.ReadFile("examples/attachment/rabbit.jpg")
+				require.NoError(t, err, "failed to read rabbit.jpg")
+		dogImg, err := os.ReadFile("examples/attachment/dog.jpg")
+				require.NoError(t, err, "failed to read dog.jpg")
+
+	// Test: add mime headers and body then send
+		subject := fmt.Sprintf("sesmailer integration test - html+embeds - %d", time.Now().UnixNano())
+		output, err := mailer.
+			SetFrom( "noreply@xeno.com.my", "Xeno System").
+			AddReplyTo( "info@xeno.com.my", "Xeno Admin" ).
+			AddTo( "elmyrockers@gmail.com", "Developer").
+			AddCC( "elmyrockers2@gmail.com", "Maintainer").
+			AddBCC( "elmyrockers3@gmail.com", "Project Manager").
+			SetSubject(subject).
+
+			SetBody("<h1>Hello</h1><p>This email has embeds.</p><img src='cid:cat'/><img src='cid:rabbit'/><img src='cid:dog'/>").AsHTML().
+			SetAltBody("Hello. This is the plain text fallback.").
+			Embed("cat.webp", catImg, "cat").
+			Embed("rabbit.jpg", rabbitImg, "rabbit").
+			Embed("dog.jpg", dogImg, "dog").
+			Send()
+
+	require.NoError(t, err, "expected Send to succeed")
+	require.NotNil(t, output)
+	assert.NotEmpty(t, output.MessageId)
+
+	t.Logf("Sent HTML+embeds email, MessageId=%s", *output.MessageId)
 }
