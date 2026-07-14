f0b037b1e62d13aa6f8d54607475dce698a9f6e3
elmyrockers <elmyrockers@gmail.com>
2026-07-14 23:27:39 +0800

    Update example code for attachment: add AttachFile() method

--- examples/attachment/main.go
+++ examples/attachment/main.go
@@ -8,23 +8,19 @@ import (
 )
 
 func main() {
-	// Get image data in bytes
+	// // Get image data in bytes
 		catImg, err := os.ReadFile("cat.webp")
-			if err != nil { log.Fatal(err) }
-		rabbitImg, err := os.ReadFile("rabbit.jpg")
-			if err != nil { log.Fatal(err) }
-		dogImg, err := os.ReadFile("dog.jpg")
 			if err != nil { log.Fatal(err) }
 
 	// Create mailer
-	sesmailer.New().
+	mailer := sesmailer.New().
 		SetFrom("no-reply@xeno.com.my", "Xeno System").
 		AddAddress("elmyrockers@gmail.com", "Helmi Aziz").
 		SetSubject("Email With Attachments").
 		SetBody("This is test email with attachments").
 		Attach( "cat.webp", catImg ). //<----------------------Add a few pictures as attachments
-		Attach( "rabbit.jpg", rabbitImg ).
-		Attach( "dog.jpg", dogImg ).
+		AttachFile( "rabbit.jpg", "rabbit.jpg" ).
+		AttachFile( "dog.jpg", "dog.jpg" ).
 		Dump().
 		Send()
 }
