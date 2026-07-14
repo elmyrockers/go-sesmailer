68b3d894b30b88abbf733fed920a07a04783c591
elmyrockers <elmyrockers@gmail.com>
2026-07-15 00:04:28 +0800

    Update example code for attachment

--- examples/attachment/main.go
+++ examples/attachment/main.go
@@ -10,17 +10,28 @@ import (
 func main() {
 	// // Get image data in bytes
 		catImg, err := os.ReadFile("cat.webp")
-			if err != nil { log.Fatal(err) }
+			if err != nil {
+				log.Fatalf("Failed to read attachment: %v", err)
+				return
+			}
 
 	// Create mailer
-	mailer := sesmailer.New().
-		SetFrom("no-reply@xeno.com.my", "Xeno System").
-		AddAddress("elmyrockers@gmail.com", "Helmi Aziz").
-		SetSubject("Email With Attachments").
-		SetBody("This is test email with attachments").
-		Attach( "cat.webp", catImg ). //<----------------------Add a few pictures as attachments
-		AttachFile( "rabbit.jpg", "rabbit.jpg" ).
-		AttachFile( "dog.jpg", "dog.jpg" ).
-		Dump().
-		Send()
+		mailer := sesmailer.New()
+
+		messageID, err := mailer.
+								SetFrom("noreply@xeno.com.my", "Xeno System").
+								AddAddress("elmyrockers@gmail.com", "Helmi Aziz").
+								SetSubject("Email With Attachments").
+								SetBody("This is test email with attachments").
+								Attach( "cat.webp", catImg ). //<----------------------Add a few pictures as attachments
+								AttachFile( "rabbit.jpg", "rabbit.jpg" ).
+								AttachFile( "dog.jpg", "dog.jpg" ).
+								Dump().
+								Send()
+    if err != nil {
+        log.Fatalf("Failed to send email:\n%v", err)
+        return
+    }
+
+    log.Println( "Email sent successfully!\nID: ", messageID )
 }
